0734538133333d0b0d689dc43ebed3f95fb88dbf
Jesus Velazquez <jesusvelazquez0980@gmail.com>
2023-04-03 19:11:52 -0400

    useren

--- geekText/src/main/java/com/geekText/geekText/Entity/User.java
+++ geekText/src/main/java/com/geekText/geekText/Entity/User.java
@@ -7,30 +7,37 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Table(name = "User")
+@Table(name = "User_DB")
+
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-
     @Id
-    @Column(name = "UserID")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int UserID;
-
-
     @Column(name = "Username")
-    private String Username;
+    private String username;
 
     @Column(name = "Password")
-    private String Password;
+    private String password;
 
-    @Column(name = "Name")
-    private String Name;
+    @Column(name = "FirstName")
+    private String firstName;
 
-    @Column(name = "e-Mail")
-    private String eMail;
+    @Column(name = "LastName")
+    private String lastName;
+
+    @Column(name = "Email")
+    private String email;
 
     @Column(name = "Address")
-    private String Address;
+    private String address;
 
+    public String getUsername(){
+        return username;
+    }
+
+   /*@OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn (name = "credit_card_number_fk")
+   private CreditCard creditCard;
+   */
 }
