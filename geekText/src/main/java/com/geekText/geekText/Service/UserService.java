b8fc231bb6b62cf27a14a974ab73fe7a771d57ad
Jesus Velazquez <jesusvelazquez0980@gmail.com>
2023-04-03 19:14:40 -0400

    userservice

--- geekText/src/main/java/com/geekText/geekText/Service/UserService.java
+++ geekText/src/main/java/com/geekText/geekText/Service/UserService.java
@@ -1,18 +1,31 @@
 package com.geekText.geekText.Service;
 
-import com.geekText.geekText.Entity.User;
-import com.geekText.geekText.Repository.UserRepo;
+import com.CEN4010.CEN4010.Entity.User;
+import com.CEN4010.CEN4010.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
-
     @Autowired
     private UserRepo userRepo;
 
-    public User saveDetails(User user){
-
+    public User save(User user)
+    {
         return userRepo.save(user);
     }
+
+    public List<User> getDetails()
+    {
+        return userRepo.findAll();
+    }
+
+    public Optional<User> findByUsername(String username)
+    {
+        return userRepo.findByUsername(username);
+    }
+
 }
